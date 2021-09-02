1e7003da9b6b807d655eb610d0e8be67f4bd91cf
Sun2548 <89177379+Sun2548@users.noreply.github.com>
2021-09-02 10:47:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Helloปนัสยา ศิลปศาสตร์  เลขที่13 ม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
